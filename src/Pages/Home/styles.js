9cf3fb2962bde278c71d0248b635a29e44bb718b
Luan Pinho <luanpinhoo@gmail.com>
2021-04-30 00:49:19 -0400

    feat/create-template: created project and installed dependencies

---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  color: aquamarine;
+  width: 100%;
+  height: 100vh;
+`;
